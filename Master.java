d208c43123c3925270d6e1c13c15f23d2b261304
maxim_stoma <maksim.stoma@mail.ru>
2023-09-29 06:48:43 +0300

    - created Student class (added base fields and a constructor); - created Master class (added base fields and a constructor);

---
+++ Master.java
@@ -0,0 +1,16 @@
+public class Master {
+
+    //region Private Fields
+    private String name;
+    private double salary = 80000;
+
+    //endregion
+
+    //region Constructors
+
+    public Master(String name) {
+        this.name = name;
+    }
+
+    //endregion
+}
---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student {
+
+    //region Private Fields
+    private String name;
+    private double salary = 2000;
+
+    //endregion
+
+    //region Constructors
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    //endregion
+}
