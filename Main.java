7f6851ccf90c2999a51fcd5b7b95e331243a457a
maxim_stoma <maksim.stoma@mail.ru>
2023-09-29 07:06:58 +0300

    -updated Main class (imitated the program work)

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Publisher jobAgency = new JobAgency();
+        Company google = new Company("Google", jobAgency, 120000);
+        Company yandex = new Company("Yandex", jobAgency, 95000);
+        Company geekBrains = new Company("GeekBrains", jobAgency, 98500);
+
+        Student student1 = new Student("Nick Dickson");
+        Master master1 = new Master("Walter Copperfield");
+        Master master2 = new Master("Grace Park");
+
+        jobAgency.registerObserver(student1);
+        jobAgency.registerObserver(master1);
+        jobAgency.registerObserver(master2);
+
+        for (int i = 0; i < 3; i++) {
+            google.needEmployee();
+            yandex.needEmployee();
+            geekBrains.needEmployee();
+        }
     }
 }
